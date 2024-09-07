249131943bd67476d24732472310f7c6ee069bef
Patryk Jura <you@example.com>
2024-09-07 19:37:47 +0200

    Added split_dataFrame

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from import_and_clean_data import import_data, clean_data
+from model import split_dataFrame
 import click
 import yaml
+
 
 @click.command()
 @click.option('--config', '-c', default='../config.yaml', help='Path to the configuration file')
@@ -10,6 +12,9 @@ def run(config) -> None:
 
         Titanic_datFrame = import_data.import_data(config_data.get('path_dataSet'))
         label_for_training, df_features_for_training =  clean_data.clean(Titanic_datFrame)
+
+        x_train, x_test, y_train, y_test = split_dataFrame.split(label_for_training, df_features_for_training)
+
 
 
 if __name__ == "__main__":
